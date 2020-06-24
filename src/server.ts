6dbdd2a0cfee899ff01cb2c02f95d84b797af31b
Jhoseph Araujo <j.garibald@gmail.com>
2020-06-24 16:10:07 -0300

    Adicionado arquivo de rotas e json no server.js

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { json } from 'express';
 import routes from './routes';
 
 const app = express();
+app.use(json());
+app.use(routes);
 
 app.get('/', (request, response) => response.json({ message: 'Server ok' }));
 
